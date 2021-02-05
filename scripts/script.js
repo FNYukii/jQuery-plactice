fd7af1361621a0d32826c85359cf9c374e3f7362
YuSan357 <forestgreen14689@gmail.com>
2021-02-05 15:57:28 +0900

    Practiced how to use a jQuery basics.

---
+++ scripts/script.js
@@ -0,0 +1,60 @@
+$(function(){
+
+  
+  // fade and slide
+  $('#image1-fadeout').click(function(){
+    $('.green-img').fadeOut();
+  });
+
+  $('#image1-fadein').click(function(){
+    $('.green-img').fadeIn();
+  });
+  
+  $('#image1-slidedown').click(function(){
+    $('.green-img').slideDown();
+  });
+
+  $('#image1-slideup').click(function(){
+    $('.green-img').slideUp();
+  });
+
+
+  // change border-radius
+  $('#green-circle').click(function(){
+    $('.green-img').animate({
+      'border-radius':'50%'
+    }, 500);
+  });
+
+  $('#green-square').click(function(){
+    $('.green-img').animate({
+      'border-radius':'0%'
+    }, 500);
+  });
+
+
+  // change selected img
+  $('#green-left').click(function(){
+    let $greenImg = $('.green-img');
+    let greenIndex = $('.img').index($greenImg);
+    $greenImg.removeClass('green-img');
+    if(greenIndex != 0){
+      $greenImg.prev().addClass('green-img');
+    }else{
+      $('.img').eq($('.img').length - 1).addClass('green-img');
+    }
+  });
+
+  $('#green-right').click(function(){
+    let $greenImg = $('.green-img');
+    let greenIndex = $('.img').index($greenImg);
+    $greenImg.removeClass('green-img');
+    if(greenIndex != $('.img').length -1){
+      $greenImg.next().addClass('green-img');
+    }else{
+      $('.img').eq(0).addClass('green-img');
+    }
+  });
+
+
+});
